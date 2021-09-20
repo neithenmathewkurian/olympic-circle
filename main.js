5c2d9f25fa8d8d923360106db4a9a6bc5d021bcc
neithenmathewkurian <86046699+neithenmathewkurian@users.noreply.github.com>
2021-09-20 09:48:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+ctx.beginPath();
+ctx.lineWidth=6;
+ctx.strokeStyle="green";
+ctx.rect(150,143,430,200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.lineWidth=6;
+ctx.strokeStyle="green";
+ctx.arc(250,200,40,0=Math.PI);
+ctx.stroke();
