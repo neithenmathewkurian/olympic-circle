a8ad62e731140dc65dfdd899e0fd4f03f0f4b006
neithenmathewkurian <86046699+neithenmathewkurian@users.noreply.github.com>
2021-09-20 21:05:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var canvas=document.getElementById("Mycanvas");
+var ctx=canvas.getContext("2d");
 ctx.beginPath();
 ctx.lineWidth=6;
 ctx.strokeStyle="green";
@@ -6,6 +8,6 @@ ctx.stroke();
 
 ctx.beginPath();
 ctx.lineWidth=6;
-ctx.strokeStyle="green";
+ctx.strokeStyle="blue";
 ctx.arc(250,200,40,0=Math.PI);
 ctx.stroke();
